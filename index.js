32fc3d68d6f56ea3ac6d9eb7833a59cdbbb8407c
getgrip <tengis0621@gmail.com>
2019-04-16 06:36:46 -0400

    original

---
+++ index.js
@@ -0,0 +1,35 @@
+//jshint esversion: 6
+
+const express = require("express"); 
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = new express(); 
+
+app.use(bodyParser.urlencoded({extended:true})); 
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat; 
+    var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+    var finalURL = baseURL + crypto +fiat; 
+    request(finalURL, function(error, response, body){
+
+        var data = JSON.parse(body);
+        var price = data.last;
+        var currentDate = data.display_timestamp; 
+
+        res.write("<p>The current date is " + currentDate + "</p>");
+        res.write("<h1>The Price of the "+ crypto +  " is " + price + " " + fiat + " </h1>");
+        res.send();
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
+
+
